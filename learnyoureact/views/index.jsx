ff8067525cc5a02d5db5ccf61550afc739f5d16a
Vanilla <mail@liyaodong.com>
2015-07-07 14:57:21 +0800

    kill learnyoureact

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,11 +1,17 @@
 var React = require('react');
+var _ = require('underscore');
 
 var TodoBox = React.createClass({
+  getInitialState: function () {
+    return {
+      data: this.props.data
+    };
+  },
   render: function() {
     return (
       <div className="todoBox">
         <h1>Todos</h1>
-        <TodoList data = {this.props.data}/>
+        <TodoList data = {this.state.data}/>
         <TodoForm/>
       </div>
     );
@@ -13,13 +19,57 @@ var TodoBox = React.createClass({
 });
 
 var TodoList = React.createClass({
-  render: function() {
-    var todo = this.props.data.map(function(obj) {
-      return <Todo key={obj.title} title={obj.title}>{obj.detail}</Todo>;
+  getDefaultProps: function() {
+    return {
+      onDelete: this.deleteTodo
+    };
+  },
+  getInitialState: function() {
+    return {
+      data: this.props.data,
+      titleValue: '',
+      detailValue: ''
+    };
+  },
+  changeTitle: function(e) {
+    this.setState({
+      titleValue: e.target.value
     });
+  },
+  changeDetail: function(e) {
+    this.setState({
+      detailValue: e.target.value
+    });
+  },
+  addTodo: function() {
+    if(this.state.titleValue.length && this.state.detailValue.length) {
+      this.setState({
+        data: this.state.data.concat({
+          title: this.state.titleValue,
+          detail: this.state.detailValue
+        })
+      });
+    }
+  },
+  deleteTodo: function (title) {
+    this.setState({
+      data: this.state.data.filter(function (item) {
+        return item.title !== title;
+      })
+    });
+  },
+  render: function() {
+    var todo = this.state.data.map(function(obj) {
+      return <Todo key={obj.title} title={obj.title} onDelete={this.deleteTodo}>{obj.detail}</Todo>;
+    }.bind(this));
 
     return (
       <div className = "todoList">
+        <div>
+          Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
+          Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
+          <button onClick={this.addTodo}>Add</button>
+        </div>
         <table style={{border: '2px solid black'}}>
           <tbody>
             {todo}
@@ -32,7 +82,8 @@ var TodoList = React.createClass({
 
 var Todo = React.createClass({
   propTypes: {
-    title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired,
+    onDelete: React.PropTypes.func.isRequired
   },
   getInitialState: function() {
     return {
@@ -45,10 +96,14 @@ var Todo = React.createClass({
     });
     console.log(e.target.checked);
   },
+  _onDelete: function () {
+    this.props.onDelete(this.props.title);
+  },
   render: function() {
     var trStyle = this.state.checked ? style.checkedTodo : style.notCheckedTodo;
     return (
       <tr style={trStyle}>
+        <td style={style.tableContent}><button onClick={this._onDelete}>X</button></td>
         <td style={style.tableContent}>
           <input checked={this.state.checked} onChange={this.handleChange} type="checkbox"/>
         </td>
