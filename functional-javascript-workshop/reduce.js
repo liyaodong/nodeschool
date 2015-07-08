474ce2934cab909653d1d880848caf0f813b19e7
Vanilla <mail@liyaodong.com>
2015-07-08 09:36:26 +0800

    can't stop copy

--- functional-javascript-workshop/reduce.js
+++ functional-javascript-workshop/reduce.js
@@ -1,10 +1,6 @@
 function countWords(inputWords) {
   return inputWords.reduce(function (prev, current, index, array) {
-    if(prev.hasOwnProperty(current)) {
-      prev[current] = prev[current] + 1;
-    } else {
-      prev[current] = 1;
-    }
+    prev[current] = ++prev[current] || 1;
     return prev;
   }, {});
 }
