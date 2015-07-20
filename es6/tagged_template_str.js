75c4b97a6f86381f6a550c2c7ceb7a6140ede5fd
Vanilla <mail@liyaodong.com>
2015-07-20 17:32:46 +0800

    kill es6

--- es6/tagged_template_str.js
+++ es6/tagged_template_str.js
@@ -1,28 +1,33 @@
-// console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
-//
-// function html(literals, ...substitutions) {
-//   var rule = [
-//     {
-//       "'": '&apos'
-//     }, {
-//       '"': '&quot'
-//     }, {
-//       '<': '&lt'
-//     }, {
-//       '>': '&gt'
-//     }, {
-//       '&': '&amp'
-//     }
-//   ];
-//
-//   var replace = (str) => {
-//     return rule.map((ele) => {
-//       var keyName = Object.keys(ele);
-//       return keyName === str ? ele[keyName] : str;
-//     });
-//   };
-//
-//   return substitutions.reduce((pre, str, index) => {
-//     return pre + literals[index] + replace(str);
-//   });
-// }
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html(literals, ...substitutions) {
+  var rule = [
+    {
+      "'": '&apos;'
+    }, {
+      '"': '&quot;'
+    }, {
+      '<': '&lt;'
+    }, {
+      '>': '&gt;'
+    }, {
+      '&': '&amp;'
+    }
+  ];
+  var replaced = substitutions.map((str) => {
+    for (let i = 0; i < rule.length; i++ ){
+      let name = Object.keys(rule[i]);
+      if(new RegExp(name).test(str)) {
+        str = str.replace(name, rule[i][name]);
+        break;
+      }
+    }
+    return str;
+  });
+
+  var str = replaced.reduce((pre, cur, index) => {
+    return pre + literals[index] + replaced[index];
+  }, '');
+
+  return str + literals[literals.length - 1];
+}
